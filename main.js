de7faa979b74f1647364d7de500654003ce531dd
TheLegendOfThePugs <j.gobbett@internode.on.net>
2016-06-01 20:28:43 +0930

    adding to main

--- main.js
+++ main.js
@@ -108,5 +108,57 @@ function onKeyUp(event)
       }*/
 }
 
+function run() {
+    context.fillStyle = "#111";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    var deltaTime = getDeltaTime();
+
+    var s = Math.sin(player.rotation);
+    var c = Math.cos(player.rotation);
+
+    var XDir = (player.directionX * c) - (player.directionY * s);
+    var YDir = (player.directionX * s) + (player.directionY * c);
+    var XVel = XDir * PLAYER_SPEED;
+    var YVel = YDir * PLAYER_SPEED;
 
 
+    player.x += XVel;
+    player.y += YVel;
+
+    player.rotation += player.angularDirection * PLAYER_TURN_SPEED;
+
+    context.save();
+    context.translate(player.x, player.y);
+    context.rotate(player.rotation);
+    context.drawImage(
+        player.image, -player.width / 2, -player.height / 2);
+    context.restore();
+
+}
+
+(function () {
+    var onEachFrame;
+    if (window.requestAnimationFrame) {
+        onEachFrame = function (cb) {
+            var _cb = function () { cb(); window.requestAnimationFrame(_cb); }
+            _cb();
+        };
+    } else if (window.mozrequestAnimationFrame) {
+        onEachFrame = function (cb) {
+            var _cb = function () {
+                cb();
+                window.mozRequestAnimationFrame(_cb);
+            }
+            _cb();
+        };
+    } else {
+        onEachFrame = function (cb) {
+            setInterval(cb, 1000 / 60);
+        }
+    }
+
+    window.onEachFrame = onEachFrame;
+})();
+
+window.onEachFrame(run);
